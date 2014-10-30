f850bb0f19c0f51f65d34f555ed8ceaa8219cb7a
pspi <pspi@users.noreply.github.com>
2014-10-30 15:07:26 +0200

    refactor(linkedlist): rename methods and fields to resemble naming used in Arrays

--- U.ts
+++ U.ts
@@ -24,61 +24,131 @@ module U {
     }
 
     export class LinkedList<T> {
-        first: ILinkedListItem<T>;
-        last: ILinkedListItem<T>;
-        size = 0;
 
-        push(data: T) {
-            var item: ILinkedListItem<T> = {
-                value: data,
-                next: null,
-                prev: this.last
-            };
-            if (this.size == 0) {
-                this.first = this.last = item;
-            } else {
-                this.last.next = item;
-                this.last = item;
-            }
-            this.size++;
+        head: ILinkedListItem<T> = null;
+        tail: ILinkedListItem<T> = null;
+        changeCount = 0; // can be used for watching changes for example in angularjs
+
+        // Array interface satisfying 
+
+        length = 0;
+        push(...items: T[]): number {
+            items.forEach((item) => {
+                var listItem: ILinkedListItem<T> = {
+                    value: item,
+                    next: null,
+                    prev: this.tail
+                };
+                if (this.length == 0) {
+                    this.head = this.tail = listItem;
+                } else {
+                    this.tail.next = listItem;
+                    this.tail = listItem;
+                }
+                this.length++;
+                this.changeCount++;
+            });
+            return this.length;
         }
 
-        clear() {
-            while (this.size > 0) {
-                this.removeFirst();
+        unshift(t: T) {
+            if (this.length == 0) {
+                throw "oh please god dont do this to empty list, aka not implemented";
             }
+            var newFirst: ILinkedListItem<T> = {
+                value: t,
+                next: this.head,
+                prev: null
+            }
+            this.head.prev = newFirst;
+            this.head = newFirst;
+
+            this.length++;
+            this.changeCount++;
+
+            return this.length;
         }
 
-        pushList(u: LinkedList<T>) {
-            u.forEach(i => this.push(i));
-        }
-
-        removeFirst(): T {
-            var value = this.first.value;
-            this.first = this.first.next;
-            this.size--;
-            if (this.size == 0) {
-                this.last = null;
+        shift(): T {
+            var value = this.head.value;
+            this.head = this.head.next;
+            this.length--;
+            if (this.length == 0) {
+                this.tail = null;
             }
+            this.changeCount++;
             return value;
         }
 
         forEach(cb: (value: T) => void) {
             var cur;
-            while (cur = (cur ? cur.next : this.first)) {
+            while (cur = (cur ? cur.next : this.head)) {
                 cb(cur.value);
             }
         }
 
+        // extended
+        clear() {
+            while (this.length > 0) {
+                this.shift();
+            }
+            this.changeCount++;
+        }
+
+        pushList(u: LinkedList<T>) {
+            u.forEach(i => this.push(i));
+            this.changeCount++;
+        }
+
+        pushArray(items: T[]) {
+            items.forEach(item => this.push(item));
+        }
+
+        replace(newList: U.LinkedList<T>) {
+            this.clear();
+            this.pushList(newList);
+        }
+
+        replaceArray(newArray: T[]) {
+            this.clear();
+            this.pushArray(newArray);
+        }
+
         isEmpty(): boolean {
-            return this.size == 0;
+            return this.length == 0;
+        }
+
+        clone(): LinkedList<T> {
+            var clone = new LinkedList<T>();
+            clone.pushList(this);
+            return clone;
         }
 
         toArray(): T[] {
-            var t = [];
-            this.forEach((i) => t.push(i));
-            return t;
+            var array = [];
+            this.forEach((item) => array.push(item));
+            return array;
         }
+
+        toJSON() {
+            return this.toArray();
+        }
+    }
+
+    export class LimitedLinkedList<T> extends LinkedList<T> {
+
+        constructor(private limit: number) {
+            super();
+        }
+
+        push(...ticks: T[]): number {
+            ticks.forEach((tick) => super.push(tick));
+            while (this.length > this.limit) {
+                this.shift();
+            }
+            return this.length;
+        }
+
     }
 
     export function areNumbers(...args) {
