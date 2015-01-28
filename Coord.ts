72c5eda4e8595739cf50d5289722a46bcd42dd2d
pspi <pspi@users.noreply.github.com>
2015-01-28 10:27:31 +0200

    feat(coord): origin constants

--- Coord.ts
+++ Coord.ts
@@ -5,6 +5,9 @@ import _ = require('underscore');
 
 module Coord {
 
+    export var ORIGIN_2D = { x: 0, y: 0 };
+    export var ORIGIN_3D = { x: 0, y: 0, z: 0 };
+
     export interface Coord2D {
         x: number;
         y: number;
@@ -16,7 +19,7 @@ module Coord {
 
     export function distance<T extends Coord2D>(a: T, b: T) {
         var t = minus(a, b);
-        return Math.sqrt(_.reduce<number, number>(_.values(t), (memo, num) => memo + num * num, 0));
+        return Math.sqrt(_.reduce<number, number>(_.values(t),(memo, num) => memo + num * num, 0));
     }
 
     export function times<T extends Coord2D>(a: T, factor: number): T {
@@ -26,7 +29,7 @@ module Coord {
     }
 
     export function minus<T extends Coord2D>(...args: T[]): T {
-        return <T> _.foldl(args, (a, b) => {
+        return <T> _.foldl(args,(a, b) => {
             var r = {};
             _.keys(a).forEach(key => r[key] = a[key] - b[key]);
             return r;
@@ -34,7 +37,7 @@ module Coord {
     }
 
     export function plus<T extends Coord2D>(...args: T[]): T {
-        return <T> _.foldl(args, (a, b) => {
+        return <T> _.foldl(args,(a, b) => {
             var r = {};
             _.keys(a).forEach(key => r[key] = a[key] + b[key]);
             return r;
